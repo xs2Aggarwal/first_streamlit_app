62fbfaf6e2ccc8d3a8366cc8bef09d77781e8055
xs2Aggarwal <136543663+xs2Aggarwal@users.noreply.github.com>
2023-06-15 06:18:48 +0530

    Update streamlit_app.py
    using pandas to pull data from S3 file

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit
+import pandas
 streamlit.title('My Parents New Healthy Diner')
 
 streamlit.header('Breakfast Menu')
@@ -7,3 +8,6 @@ streamlit.text('🥗 Kale, Spinach & Rocket Smoothy')
 streamlit.text('🐔 Hard-Boiled Free-range Eggs')
 streamlit.text('🥑🍞Avacado Toast')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
