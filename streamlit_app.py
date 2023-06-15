d96fd673f7e526f3e79615178e1f9bd6b85adef5
xs2Aggarwal <136543663+xs2Aggarwal@users.noreply.github.com>
2023-06-15 06:13:47 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ streamlit.title('My Parents New Healthy Diner')
 
 streamlit.header('Breakfast Menu')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
-streamlit.text('🥗 ale, Spinach & Rocket Smoothy')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothy')
 streamlit.text('🐔 Hard-Boiled Free-range Eggs')
 streamlit.text('🥑🍞Avacado Toast')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
