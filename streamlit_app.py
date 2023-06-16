df4f716dc82f8da1a81fbb1d8feb0f9825c12258
xs2Aggarwal <136543663+xs2Aggarwal@users.noreply.github.com>
2023-06-16 22:24:17 +0530

    Update streamlit_app.py
    added variables for user input

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,9 +20,13 @@ streamlit.dataframe(fruits_to_show)
 
 #New Section to display fruitvice api response
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+"kiwi")
+#fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+"kiwi")
 
 streamlit.header("Fruityvice Fruit Advice!")
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
+
 #streamlit.text(fruityvice_response.json()) #writes raw data
 
 #normalize the data json response
